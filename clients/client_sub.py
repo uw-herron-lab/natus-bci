2e3625b8cd44dc143a98956c6495efed6030e8a6
Daniel Wang <danielwang1590@gmail.com>
2024-07-11 10:10:53 -0700

    plot data and get channel names

--- clients/client_sub.py
+++ clients/client_sub.py
@@ -1,9 +1,11 @@
 import zmq
 import pickle
 import struct
+from zmq import Socket
+import time
 
 class ClientSub():
-    def __init__(self, sub_ip="localhost", sub_port=1000, sub_topic="ProcessedData"):
+    def __init__(self, sub_ip: str = "localhost", sub_port: int = 6000, req_port: int = 6001, sub_topic: str = "ProcessedData"):
         """Class constructor
         Args:
             sub_ip (str, optional): IP addr of the data source. Defaults to "localhost".
@@ -13,10 +15,39 @@ class ClientSub():
         self.sub_ip = sub_ip
         self.sub_port = sub_port
         self.sub_topic = sub_topic
-        self.context = zmq.Context()
-        self.sub_socket = self.context.socket(zmq.SUB)
-        self.sub_socket.connect("tcp://{}:{}".format(self.sub_ip, self.sub_port))
-        self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, self.sub_topic)
+        self.req_port = req_port
+
+        self.context: zmq.Context = zmq.Context()
+        
+        try:
+            self.sub_socket: Socket = self.context.socket(zmq.SUB)
+            self.sub_socket.connect("tcp://{}:{}".format(self.sub_ip, self.sub_port))
+            self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, self.sub_topic)
+            print(f"Connected SUB socket to tcp://{self.sub_ip}:{self.sub_port} with topic '{self.sub_topic}'")
+        except Exception as e:
+            print(f"Error connecting SUB socket: {e}")
+
+        try:
+            self.req_socket: Socket = self.context.socket(zmq.REQ)
+            self.req_socket.connect("tcp://{}:{}".format(self.sub_ip, self.req_port))
+            print(f"Connected REQ socket to tcp://{self.sub_ip}:{self.req_port}")
+        except Exception as e:
+            print(f"Error connecting REQ socket: {e}")
+        
+        self.ch_names = None
+
+    def get_channel_names(self):
+        while self.ch_names is None:
+            try:
+                print("Requesting channel names...")
+                self.req_socket.send("get_channel_names".encode())
+                ch_names = self.req_socket.recv_string()
+                self.ch_names = ch_names.split('\n')
+                print("Sent request!")
+                # print(f"Received channel names: {self.ch_names}")
+            except Exception as e:
+                print(f"Error getting channel names: {e}")
+                time.sleep(0.1)
 
     def get_data(self):
         try:
@@ -47,4 +78,5 @@ if __name__ == "__main__":
 
     finally:
         client.sub_socket.close()
+        client.req_socket.close()
         client.context.term()
